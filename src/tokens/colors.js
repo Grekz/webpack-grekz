163842005509b08a3af93dd3ec66c74eb93ecbdf
Grekz <vidbla.grekz@gmail.com>
2018-06-25 21:49:58 +0200

    Adding mexican palette

--- src/tokens/colors.js
+++ src/tokens/colors.js
@@ -5,6 +5,13 @@ const colors = {
       hover: '#3ddad7',
       active: '#135589'
     }
+  },
+  palette: {
+    $color1: rgba(0, 104, 69, 1),
+    $color2: rgba(216, 169, 104, 1),
+    $color3: rgba(44, 195, 221, 1),
+    $color4: rgba(252, 229, 233, 1),
+    $color5: rgba(206, 8, 31, 1)
   }
 }
 
