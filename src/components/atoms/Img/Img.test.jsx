c7d6782f24d66156c082885e2c2c1dd0928f2f34
Grekz <vidbla.grekz@gmail.com>
2018-06-27 21:51:34 +0200

    Removing unused imports and linting on files

--- src/components/atoms/Img/Img.test.jsx
+++ src/components/atoms/Img/Img.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Img from './Img'
-import { configure, shallow, mount } from 'enzyme'
+import { configure, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 
@@ -8,15 +8,15 @@ configure({ adapter: new Adapter() })
 
 describe('<Img /> rendering', () => {
   it('should render Img', () => {
-    const wrapper = shallow(<Img />)
+    const wrapper = mount(<Img />)
     expect(wrapper).toHaveLength(1)
   })
   it('should contain 1 img element', () => {
-    const wrapper = shallow(<Img />)
+    const wrapper = mount(<Img />)
     expect(wrapper.find('img')).toHaveLength(1)
   })
   it('mathces the snapshot', () => {
-    const tree = shallow(<Img />)
+    const tree = mount(<Img />)
     expect(toJson(tree)).toMatchSnapshot()
   })
 })
